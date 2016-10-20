1d57141e74ae5df56ea72c95263314e9a7a40ead
Peng Li <penli1@users.noreply.github.com>
2016-10-20 11:13:19 +0800

    Create deploy_metrics.sh

---
+++ deploy_metrics.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export PREFIX=
+export PROJECT=openshift-infra
+export SUBDOMAIN=
+export MASTER_URL=
+
+
+oc project ${PROJECT}
+
+oc create serviceaccount metrics-deployer
+
+oadm policy add-cluster-role-to-user cluster-reader system:serviceaccount:openshift-infra:heapster
+
+oc policy add-role-to-user edit system:serviceaccount:openshift-infra:metrics-deployer
+
+oc secrets new metrics-deployer nothing=/dev/null
+
+oc new-app metrics-deployer-template --as=system:serviceaccount:openshift-infra:metrics-deployer -p \
+IMAGE_PREFIX=${PREFIX},\
+IMAGE_VERSION=,\
+MASTER_URL=${MASTER_URL},\
+HAWKULAR_METRICS_HOSTNAME=hawkular-metrics.${SUBDOMAIN},\
+MODE=deploy,\
+USE_PERSISTENT_STORAGE=true,\
+DYNAMICALLY_PROVISION_STORAGE=true,\
+CASSANDRA_NODES=1,\
+CASSANDRA_PV_SIZE=10Gi
